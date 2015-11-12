a8cfe861b5f605d8eb1ef8b074dabb5562c62e11
Murray <1232200@sms.ed.ac.uk>
2015-11-12 14:17:49 +0000

    Fixed cropping and scaling to be more robust. Began work on background to show behind image being worked on. Made thresholding user-controlable.

--- PROJ/main.py
+++ PROJ/main.py
@@ -39,32 +39,63 @@ class Master(object):
         self.open_file_button = Button(self.root, text="Open File", command=self.open_file)
         self.open_file_button.grid(row=1, column=3+PN_W+SN_W)
 
-        self.image_frame = Label(self.root)
-        self.image_frame.grid(row=2, column=0+PN_W+SN_W, columnspan=IE_W)
+        self.thresh_slide = Scale(self.root, from_=1, to=255, orient=HORIZONTAL, label="Threshold", showvalue=0,
+                                  command=self.thresh_slide_update, length=200)
+        self.thresh_slide.set(200)
+        self.thresh_slide.grid(row=1, column=1+PN_W+SN_W)
+
+        self.image_wrapper = Label(self.root, image='')
+        self.image_wrapper.grid(row=2, column=0+PN_W+SN_W, columnspan=IE_W)
+        self.image_frame = Label(self.image_wrapper, bg='#ff00ff')
+        self.image_frame.pack()
         self.image_frame.bind("<Button-1>", self.image_clicked)
+        self.image_orig = None
         self.image_cv = None
         self.image_tk = None
 
         self.regions = []
         self.points = []
+        self.crop_points = None
+        self.scale_ratio = 1.0
+
         self.root.bind('<q>', lambda e: self.add_area())
         self.root.bind('<c>', lambda e: self.crop())
 
-        self.crop_flag = False
-
     def image_clicked(self, event):
-        self.points.append((event.x, event.y))
-        print "clicked at", event.x, event.y
+
+        # get relative co-ordinates on full image
+        x = int(event.x / self.scale_ratio)
+        y = int(event.y / self.scale_ratio)
+
+        # if cropped, offset
+        if self.crop_points:
+            x1, y1, x2, y2 = self.crop_points
+            x += x1
+            y += y1
+
+        self.points.append((x, y))
+        print "clicked at", event.x, event.y, "translated to", x, y
 
     def selection_complete(self):
         print "area defined"
 
     def crop(self):
+        if len(self.points) >= 2:
+            x1, y1 = self.points[0]
+            x2, y2 = self.points[1]
 
-        self.crop_flag = True
-        self.refresh_image()
+            print "crop:", x1, x2, y1, y2
 
-        print "crop"
+            self.points = []
+
+            self.crop_points = (x1, y1, x2, y2)
+
+            self.refresh_image()
+        else:
+            print "not enough points to crop, resetting crop"
+            self.points = []
+            self.crop_points = None
+            self.refresh_image()
 
     def add_area(self):
         print "add area"
@@ -81,40 +112,50 @@ class Master(object):
         name = askopenfilename(**self.file_options)
         self.file_options['initialdir'] = name.rsplit('/', 1)[0]
 
-        cvimg = cv2.imread(name)
-        blur = cv2.GaussianBlur(cvimg, (5, 5), 0)
-        blur_gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
-        retval, thresh_gray = cv2.threshold(blur_gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-        thresh_gray_inv = cv2.bitwise_not(thresh_gray)
-        cvimg = cv2.bitwise_and(cvimg, cvimg, mask=thresh_gray_inv)
+        self.image_orig = cv2.imread(name)
 
-        b, g, r = cv2.split(cvimg)
-        cvimg = cv2.merge((r, g, b, thresh_gray_inv))
+        self.image_cv = thresh_image(self.image_orig, self.thresh_slide.get())
+        self.refresh_image()
 
-        self.image_cv = cvimg
+    def thresh_slide_update(self, xd):
+        print xd
+        self.image_cv = thresh_image(self.image_orig, self.thresh_slide.get())
         self.refresh_image()
 
     def refresh_image(self):
-        # scale
-        shape = self.image_cv.shape
-        width = shape[1]
-        scale_ratio = 640 / float(width)
-        scaled = cv2.resize(self.image_cv, (0, 0), fx=scale_ratio, fy=scale_ratio)
+        display = self.image_cv
 
-        # crop
-        if self.crop_flag:
-            self.crop_flag = False
+        if self.crop_points:
+            x1, y1, x2, y2 = self.crop_points
+            # crop
             # img[y: y + h, x: x + w]
-            x1, y1 = self.points[0]
-            x2, y2 = self.points[1]
-            self.points = []
-            scaled = scaled[y1:y2, x1:x2]
+            display = display[y1:y2, x1:x2]
+            self.scale_ratio = calc_scale_ratio(display)
+            # scale
+            display = cv2.resize(display, (0, 0), fx=self.scale_ratio, fy=self.scale_ratio)
+        else:
+            self.scale_ratio = calc_scale_ratio(self.image_cv)
+            # scale
+            display = cv2.resize(display, (0, 0), fx=self.scale_ratio, fy=self.scale_ratio)
 
-        img = Image.fromarray(scaled)
+        img = Image.fromarray(display)
         img_tk = ImageTk.PhotoImage(image=img)
         self.image_tk = img_tk
         self.image_frame.configure(image=self.image_tk)
 
+def thresh_image(image, thresh_val):
+    blur = cv2.GaussianBlur(image, (5, 5), 0)
+    blur_gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
+    retval, thresh_gray = cv2.threshold(blur_gray, thresh_val, 255, cv2.THRESH_BINARY_INV)
+    threshed = cv2.bitwise_and(image, image, mask=thresh_gray)
+
+    b, g, r = cv2.split(threshed)
+    return cv2.merge((r, g, b, thresh_gray))
+
+def calc_scale_ratio(image):
+    shape = image.shape
+    width = shape[1]
+    return 640 / float(width)
 
 if __name__ == '__main__':
     master = Master()
