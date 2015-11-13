f03ff20b0a7a7f1dc9f45a9af075b843878f073c
Murray <1232200@sms.ed.ac.uk>
2015-11-13 16:03:03 +0000

    Added erase functionality. Implemented basic undo/redo, still needs work.

--- PROJ/main.py
+++ PROJ/main.py
@@ -3,7 +3,7 @@ import numpy as np
 from Tkinter import *
 from tkFileDialog import *
 from PIL import Image, ImageTk
-from multiprocessing import Process
+from itertools import izip, tee
 
 """
 TODO:
@@ -13,19 +13,25 @@ TODO:
 -have clickevents on image recorded
 -have 4 clickevents define a crop area, set everything outside of this to transparent
 -ability to have multiple crop areas, with more than 4 points, everything outside of *all* of these is set to transparent
+
+-store history of operations + relevant data eg: [('crop', (x1, y1, x2, y2)), ('crop', (x3, y3, x4, y4))] to be able to undo/redo in a queue fashion
 """
 
 PN_W = 1  # page nav width
 SN_W = 1  # slide nav width
 IE_W = 5  # image edit width
 
-POINT_WIDTH = 2
 POINT_SCALE = 6
+POINT_LINE_WIDTH = 2
 POINT_CLICK_PADDING = 5
 POINT_COLOUR = (255, 0, 255, 255)
 
 LINE_WIDTH = 2
 LINE_COLOUR = (128, 0, 128, 255)
+
+NO_COLOUR = (0, 0, 0, 0)
+
+DEBUG_INVALID = "invalid number of points!"
 
 class Master(object):
     def __init__(self):
@@ -68,14 +74,18 @@ class Master(object):
         self.crop_points = None
         self.scale_ratio = 1.0
 
-        self.current_op = StringVar()
-        self.current_op.set("")
+        self.op_current = StringVar()
+        self.op_current.set("")
+        self.op_history = []
+        self.op_history_index = -1
 
-        self.current_op_label = Label(self.root, textvariable=self.current_op)
+        self.current_op_label = Label(self.root, textvariable=self.op_current)
         self.current_op_label.grid(row=1, column=2+PN_W+SN_W)
 
-        self.root.bind('<q>', lambda e: self.add_area())
+        self.root.bind('<e>', lambda e: self.erase())
         self.root.bind('<c>', lambda e: self.crop())
+        self.root.bind('<z>', lambda e: self.undo())
+        self.root.bind('<y>', lambda e: self.redo())
 
         # debug
         self.root.bind('<r>', lambda e: self.refresh_image())
@@ -83,15 +93,24 @@ class Master(object):
         self.running = False  # Tkinter scales are strange
 
     def escape(self):
-        if self.current_op.get() == "":
+        op = self.op_current.get()
+        if op == "":
             if len(self.points) > 0:
                 self.points = []
                 self.refresh_image()
             else:
                 self.root.quit()
+        elif op == "crop" or op == "erase":
+            if len(self.points) > 0:
+                self.points = []
+                self.refresh_image()
+            else:
+                self.op_current.set("")
+                self.points = []
+                self.refresh_image()
         else:
-            self.current_op.set("")
-            self.reset_points()
+            self.op_current.set("")
+            self.points = []
             self.refresh_image()
 
     def image_clicked(self, event):
@@ -106,7 +125,7 @@ class Master(object):
             x += x1
             y += y1
 
-        size = self.point_size() + POINT_CLICK_PADDING
+        size = self.scale_constant(POINT_SCALE) + POINT_CLICK_PADDING
         new_point = True
         # check if spot clicked is already a point
         for index, pt in enumerate(self.points):
@@ -118,24 +137,27 @@ class Master(object):
 
         accept_point = True
         if new_point:
-            if self.current_op.get() == "crop":
+            if self.op_current.get() == "crop":
                 if len(self.points) >= 2:
-                    accept_point = False
+                    self.points.pop()
 
             if accept_point:
                 self.points.append((x, y))
 
         self.refresh_image()
 
-    def selection_complete(self):
-        print "area defined"
-
     def crop(self):
         # stage 2 of crop
-        if self.current_op.get() == "crop":
+        if self.op_current.get() == "crop":
+
+            # store current crop
+            if self.crop_points:
+                prev = self.crop_points
+            else:
+                prev = None
+
             if len(self.points) == 2:
                 print "successfully cropped"
-                # finalize the crop
                 xa, ya = self.points[0]
                 xb, yb = self.points[1]
                 if xa <= xb:
@@ -150,23 +172,30 @@ class Master(object):
                 else:
                     y1 = yb
                     y2 = ya
-                self.reset_points()
+
+                self.points = []
                 self.crop_points = (x1, y1, x2, y2)
-                self.current_op.set("")
-                self.refresh_image()
+                self.op_current.set("")
+
+                self.op_history_add({'op': 'crop',
+                                     'prev': prev,
+                                     'curr': self.crop_points})
+
             elif len(self.points) == 0:
                 print "resetting crop"
-                self.reset_points()
-                self.current_op.set("")
-                self.refresh_image()
+                self.crop_points = None
+                self.op_current.set("")
+
+                self.op_history_add({'op': 'crop',
+                                     'prev': prev,
+                                     'curr': self.crop_points})
             else:
-                print "invalid number of points!"
-                self.refresh_image()
+                print DEBUG_INVALID
 
         # stage 1 of crop
-        elif self.current_op.get() == "":
+        elif self.op_current.get() == "":
             self.points = []
-            self.current_op.set("crop")
+            self.op_current.set("crop")
             print "please enter [0|2] points and hit C"
 
         # we have interrupted some other operation
@@ -175,13 +204,64 @@ class Master(object):
 
         self.refresh_image()
 
-    def add_area(self):
-        print "add area"
+    def erase(self):
+        if self.op_current.get() == "erase":
+            if len(self.points) > 2:
+                print "successfully erased"
+                np_points = np.array([self.points])
+                cv2.fillPoly(self.image_cv, np_points, NO_COLOUR)
+                self.points = []
+                self.op_current.set("")
+            elif len(self.points) == 0:
+                self.op_current.set("")
+            else:
+                print DEBUG_INVALID
+
+        elif self.op_current.get() == "":
+            self.points = []
+            self.op_current.set("erase")
+            print "please define an area and hit E"
+
+        else:
+            pass
+
+        self.refresh_image()
+
+    def op_history_add(self, data):
+        # TODO: broken? undo-op-undo/redo kills things
+        # discard anything that could be re-done
+        if self.op_history_index < len(self.op_history)-1:
+            self.op_history = self.op_history[:self.op_history_index]
+
+        self.op_history.append(data)
+        self.op_history_index = len(self.op_history)-1
+
+    def undo(self):
+        if self.op_history_index >= 0:
+            last_op = self.op_history[self.op_history_index]
+
+            if last_op['op'] == "crop":
+                self.crop_points = last_op['prev']
+
+            self.op_history_index -= 1
+            self.refresh_image()
+
+    def redo(self):
+        if self.op_history_index < len(self.op_history)-1:
+            self.op_history_index += 1
+            next_op = self.op_history[self.op_history_index]
+
+            if next_op['op'] == "crop":
+                self.crop_points = next_op['curr']
+
+            self.refresh_image()
 
     def run(self):
         try:
             self.root.mainloop()
         except:
+            self.reset_all()
+            self.refresh_image()
             raise
         finally:
             print "Exiting..."
@@ -191,11 +271,11 @@ class Master(object):
         self.file_options['initialdir'] = name.rsplit('/', 1)[0]
 
         self.image_orig = cv2.imread(name)
-        self.reset_points()
+        self.reset_all()
         self.image_cv = thresh_image(self.image_orig, self.thresh_slide.get())
         self.refresh_image()
 
-    def reset_points(self):
+    def reset_all(self):
         self.points = []
         self.regions = []
         self.crop_points = None
@@ -225,8 +305,9 @@ class Master(object):
         self.scale_ratio = calc_scale_ratio(display)
 
         # draw anything else on top of the base image
-        # scale for drawings
-        size = self.point_size()
+        # scale to match crop area
+        size = self.scale_constant(POINT_SCALE)
+        thickness = self.scale_constant(POINT_LINE_WIDTH)
 
         # draw crosses to show points
         for p_x, p_y in self.points:
@@ -234,11 +315,12 @@ class Master(object):
             r_x = p_x-x_offset
             r_y = p_y-y_offset
 
-            cv2.line(display, (r_x-size, r_y-size), (r_x+size, r_y+size), POINT_COLOUR, int(POINT_WIDTH/self.scale_ratio))
-            cv2.line(display, (r_x-size, r_y+size), (r_x+size, r_y-size), POINT_COLOUR, int(POINT_WIDTH/self.scale_ratio))
+            cv2.line(display, (r_x-size, r_y-size), (r_x+size, r_y+size), POINT_COLOUR, thickness)
+            cv2.line(display, (r_x-size, r_y+size), (r_x+size, r_y-size), POINT_COLOUR, thickness)
 
         # depending on current operation, draw lines between points
-        if self.current_op.get() == "crop":
+        op = self.op_current.get()
+        if op == "crop":
             if len(self.points) == 2:
                 x1, y1 = self.points[0]
                 x2, y2 = self.points[1]
@@ -247,10 +329,15 @@ class Master(object):
                 y1 -= y_offset
                 y2 -= y_offset
 
-                cv2.line(display, (x1, y1), (x2, y1), LINE_COLOUR, int(LINE_WIDTH/self.scale_ratio))
-                cv2.line(display, (x2, y1), (x2, y2), LINE_COLOUR, int(LINE_WIDTH/self.scale_ratio))
-                cv2.line(display, (x2, y2), (x1, y2), LINE_COLOUR, int(LINE_WIDTH/self.scale_ratio))
-                cv2.line(display, (x1, y2), (x1, y1), LINE_COLOUR, int(LINE_WIDTH/self.scale_ratio))
+                thickness = self.scale_constant(LINE_WIDTH)
+                cv2.line(display, (x1, y1), (x2, y1), LINE_COLOUR, thickness)
+                cv2.line(display, (x2, y1), (x2, y2), LINE_COLOUR, thickness)
+                cv2.line(display, (x2, y2), (x1, y2), LINE_COLOUR, thickness)
+                cv2.line(display, (x1, y2), (x1, y1), LINE_COLOUR, thickness)
+        elif op == "erase":
+            if len(self.points) > 2:
+                np_points = np.array([self.points])
+                cv2.fillPoly(display, np_points, LINE_COLOUR)
 
         # scale
         display = cv2.resize(display, (0, 0), fx=self.scale_ratio, fy=self.scale_ratio)
@@ -260,8 +347,8 @@ class Master(object):
         self.image_tk = img_tk
         self.image_frame.configure(image=self.image_tk)
 
-    def point_size(self):
-        return int(POINT_SCALE/self.scale_ratio)
+    def scale_constant(self, constant):
+        return int(constant/self.scale_ratio)
 
 
 def thresh_image(image, thresh_val):
@@ -277,6 +364,12 @@ def calc_scale_ratio(image):
     shape = image.shape
     width = shape[1]
     return 640 / float(width)
+
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return izip(a, b)
 
 if __name__ == '__main__':
     master = Master()
